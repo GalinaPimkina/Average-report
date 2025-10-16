19ae20e90a4bc7010aad61a541e58525d60949e5
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-10-16 16:49:08 +0300

    add argument '--files' to parser

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Get list of files names')
+parser.add_argument(
+    '--files',
+    nargs='+',
+    type=str,
+    help='provide a file name',
+    required=True,
+)
