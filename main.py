3631209456bc03458c3401c37e7877b69ad1e600
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-10-16 16:58:35 +0300

    create function get_products_data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 
 
 def get_one_type(string):
@@ -21,3 +22,17 @@ parser.add_argument(
 )
 
 data = parser.parse_args()
+files = data.files
+report_type = data.report
+
+
+def get_products_data(files: list):
+    products_data = {}
+
+    for d in files:
+        with open(d, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                products_data[row['name']] = {'brand': row['brand'], 'price': int(row['price']), 'rating': float(row['rating'])}
+
+    return products_data
